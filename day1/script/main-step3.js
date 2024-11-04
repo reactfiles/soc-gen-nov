93ba11e1d5c3e87874cad3bd451e9247128c027e
React Lab Files <95652257+reactfiles@users.noreply.github.com>
2024-11-04 11:26:36 +0530

    Add files via upload

--- day1/script/main-step3.js
+++ day1/script/main-step3.js
@@ -1,12 +1,16 @@
-function clickHandler(){
+function addBoxHandler(){
     // alert("you clicked the button");
     var element = document.createElement("div");
     element.setAttribute("class","box");
     document.getElementsByClassName("container")[0].appendChild(element);
 }
+function removeBoxHandler(){
+    document.getElementsByTagName("button")[0].removeEventListener("click",addBoxHandler);
+}
 function init(){
     // alert("dom is ready");
-    document.getElementsByTagName("button")[0].addEventListener("click",clickHandler);
+    document.getElementsByTagName("button")[0].addEventListener("click",addBoxHandler);
+    document.getElementsByTagName("button")[1].addEventListener("click",removeBoxHandler);
 };
 
 document.addEventListener("DOMContentLoaded", init);
